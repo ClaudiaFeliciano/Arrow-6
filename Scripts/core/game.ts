0f1ceaa48f2b810da1bf8c97cd5e7b65fb02ca81
Carlos Zabaleta <zabaletacopa@gmail.com>
2019-02-11 21:17:10 -0500

    Fixing bugs

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,89 +1,83 @@
 //IIFE - Immediately Invoked Function Expression
-(function () {
-    // game variables
-    let canvas: HTMLCanvasElement;
-    let stage: createjs.Stage;
-    let assetManager: createjs.LoadQueue; //hold everything that i want to load ( load the array of the manifest)
-    let currentScene: objects.Scene;
-    let currentState: config.Scene;
+(function() {
+  // game variables
+  let canvas: HTMLCanvasElement;
+  let stage: createjs.Stage;
+  let assetManager: createjs.LoadQueue; //hold everything that i want to load ( load the array of the manifest)
+  let currentScene: objects.Scene;
+  let currentState: config.Scene;
 
-    let assetManifest = [
-        { id: "startButton", src: "/Assets/images/startButton.png" }, //id le pongo el nombre k desee
-        { id: "restartButton", src: "/Assets/images/restartButton.png" },
-        { id: "plane", src: "/Assets/images/plane.png" },
-        { id: "meteor", src: "/Assets/images/meteor.png" },
-        { id: "island", src: "/Assets/images/island.png" },
-        { id: "space", src: "/Assets/images/space.png" },
-        { id: "engineSound", src: "/Assets/audio/engine.ogg" },
-        { id: "bulletSound", src: "/Assets/audio/bullet.ogg" }
-        // { id: "arrowsound", src: "/Assets/audio/arrowsound.mpeg" }
+  let assetManifest = [
+    { id: "startButton", src: "/Assets/images/startButton.png" }, //id le pongo el nombre k desee
+    { id: "restartButton", src: "/Assets/images/restartButton.png" },
+    { id: "plane", src: "/Assets/images/plane.png" },
+    { id: "meteor", src: "/Assets/images/meteor.png" },
+    { id: "island", src: "/Assets/images/island.png" },
+    { id: "space", src: "/Assets/images/space.png" },
+    { id: "engineSound", src: "/Assets/audio/engine.ogg" },
+    { id: "bulletSound", src: "/Assets/audio/bullet.mp3" }
+    // { id: "arrowsound", src: "/Assets/audio/arrowsound.mpeg" }
+  ]; // Basycally an array struct. Placeholder
 
-    ]; // Basycally an array struct. Placeholder
+  function Init(): void {
+    assetManager = new createjs.LoadQueue(); //create the asset manager
+    managers.Game.assetManager = assetManager; // creates a reference to the global assetManager
+    assetManager.installPlugin(createjs.Sound); //asset manager can load sound- enable sound preloading
+    assetManager.loadManifest(assetManifest); //Loads the manifest defined above -preloads all assets listed in the manifest
+    assetManager.on("complete", Start, this); // call Start when assets are finished loading
+  }
 
+  function Start(): void {
+    console.log(`%c Game Started...`, "color: blue; font-size: 20px;");
+    canvas = document.getElementsByTagName("canvas")[0];
+    stage = new createjs.Stage(canvas);
+    managers.Game.stage = stage; // passing a reference to the stage globally
+    stage.enableMouseOver(20);
+    createjs.Ticker.framerate = 60; // game will run at 60fps
+    createjs.Ticker.on("tick", Update);
 
-    function Init(): void {
-        assetManager = new createjs.LoadQueue(); //create the asset manager
-        managers.Game.assetManager = assetManager; // creates a reference to the global assetManager
-        assetManager.installPlugin(createjs.Sound); //asset manager can load sound- enable sound preloading
-        assetManager.loadManifest(assetManifest); //Loads the manifest defined above -preloads all assets listed in the manifest
-        assetManager.on("complete", Start, this); // call Start when assets are finished loading
+    currentState = config.Scene.START;
+    managers.Game.currentState = currentState; //it makes a refrence to my stage and I hold it in my global object
+    Main();
+  }
+
+  // this is the main game loop
+  function Update(): void {
+    currentScene.Update();
+    // if the scene that is playing returns anthr scene
+    //then call Main again and switch the scene
+    if (currentState != managers.Game.currentState) {
+      currentState = managers.Game.currentState;
+      Main();
     }
 
-    function Start(): void {
-        console.log(`%c Game Started...`, "color: blue; font-size: 20px;");
-        canvas = document.getElementsByTagName("canvas")[0];
-        stage = new createjs.Stage(canvas);
-        managers.Game.stage = stage; // passing a reference to the stage globally
-        stage.enableMouseOver(20);
-        createjs.Ticker.framerate = 60; // game will run at 60fps
-        createjs.Ticker.on("tick", Update);
+    stage.update(); //redraws the state
+  }
 
-        currentState = config.Scene.START;
-        managers.Game.currentState = currentState;//it makes a refrence to my stage and I hold it in my global object
-        Main();
+  function Main(): void {
+    //remove all current objects from the stage
+    if (currentScene) {
+      currentScene.Destroy();
+      stage.removeAllChildren();
     }
 
-    // this is the main game loop
-    function Update(): void {
-
-        currentScene.Update();
-        // if the scene that is playing returns anthr scene
-        //then call Main again and switch the scene
-        if (currentState != managers.Game.currentState) {
-            currentState = managers.Game.currentState;
-            Main();
-        }
-
-        stage.update(); //redraws the state
-
+    switch (currentState) {
+      case config.Scene.START:
+        //instattiate a new scene object
+        currentScene = new scenes.Start();
+        break;
+      case config.Scene.PLAY:
+        //instattiate a new scene object
+        currentScene = new scenes.Play();
+        break;
+      case config.Scene.OVER:
+        //instattiate a new scene object
+        currentScene = new scenes.Over();
+        break;
     }
+    //add the new scene object to stage
+    stage.addChild(currentScene);
+  }
 
-    function Main(): void {
-
-        //remove all current objects from the stage
-        if (currentScene) {
-            currentScene.Destroy();
-            stage.removeAllChildren();
-        }
-
-        switch (currentState) {
-            case config.Scene.START:
-                //instattiate a new scene object
-                currentScene = new scenes.Start();
-                break;
-            case config.Scene.PLAY:
-                //instattiate a new scene object
-                currentScene = new scenes.Play();
-                break;
-            case config.Scene.OVER:
-                //instattiate a new scene object
-                currentScene = new scenes.Over();
-                break;
-        }
-        //add the new scene object to stage
-        stage.addChild(currentScene);
-
-    }
-
-    window.addEventListener("load", Init);
+  window.addEventListener("load", Init);
 })();
--- Scripts/managers/collision.ts
+++ Scripts/managers/collision.ts
@@ -1,33 +1,32 @@
 module managers {
-    export class Collision { //this laass is gonna be a container
+  export class Collision {
+    //this laass is gonna be a container
 
-        public static Check(object1: objects.AbstractGameObject, object2: objects.AbstractGameObject) {
-            let P1: math.Vec2 = new math.Vec2(object1.x, object1.y);
-            let P2: math.Vec2 = new math.Vec2(object2.x, object2.y);
-            console.log("it is in");
+    public static Check(
+      object1: objects.AbstractGameObject,
+      object2: objects.AbstractGameObject
+    ) {
+      let P1: math.Vec2 = new math.Vec2(object1.x, object1.y);
+      let P2: math.Vec2 = new math.Vec2(object2.x, object2.y);
 
-            if (math.Vec2.Distance(P1, P2) < (object1.HalfWidth + object2.HalfWidth)) {
-               console.log("xgbhxfg");
-                if (!object2.isColliding) {
-                    object2.isColliding = true;
-                    switch (object2.name) {
-                        case "island":
-                            createjs.Sound.play("bulletSound");
-                            managers.Game.scoreBoard.Score +=100;
-                            break;
-                        case "meteor":
-                            createjs.Sound.play("bulletSound");
-                            managers.Game.scoreBoard.Lives -=1;
-                            break;
-                    }
-                }
-            }
-            else {
-                object2.isColliding = false;
-               console.log("else xgbhxfg");
-
-            }
-
+      if (math.Vec2.Distance(P1, P2) < object1.HalfWidth + object2.HalfWidth) {
+        if (!object2.isColliding) {
+          object2.isColliding = true;
+          console.log("Collision detected!!!");
+          switch (object2.name) {
+            case "island":
+              createjs.Sound.play("bulletSound");
+              managers.Game.scoreBoard.Score += 100;
+              break;
+            case "meteor":
+              createjs.Sound.play("bulletSound");
+              managers.Game.scoreBoard.Lives -= 1;
+              break;
+          }
         }
+      } else {
+        object2.isColliding = false;
+      }
     }
+  }
 }
