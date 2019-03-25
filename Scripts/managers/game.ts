12e3e6ab2b6156f18266c7c8d6a9bccceec45dc7
Claudia <claudiatc1987@gmail.com>
2019-03-25 11:06:56 -0400

    Testing Bijesh

--- Scripts/managers/game.ts
+++ Scripts/managers/game.ts
@@ -1,6 +1,5 @@
 module managers {
   export class Game {
-    //esta clase la cree para tener todos las variables at the global level y poder hacer referencia desde the scene
     // Globals variables
     public static assetManager: createjs.LoadQueue; //access to all my access
     public static stage: createjs.Stage; //access to my global stage object
